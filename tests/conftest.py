b99d7a50e2f9915e9d76c9a97de7d1642bb956a6
Haitham Ghaida <haitham.ghaida@protonmail.com>
2024-10-08 13:48:57 +0200

    Initial Commit

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+"""Fixtures for trash"""
+
+# import pytest
---
+++ trash/__init__.py
@@ -0,0 +1,8 @@
+"""trash."""
+
+__all__ = (
+    "__version__",
+    # Add functions and variables you want exposed in `trash.` namespace here
+)
+
+__version__ = "0.0.1"
