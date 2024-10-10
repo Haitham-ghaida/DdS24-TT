061393e985ac5e42ab38f513b505588521d71d8c
arblastertps <t.p.s.arblaster@cml.leidenuniv.nl>
2024-10-10 14:52:32 +0200

    update URIs

--- trash/missingUris/add_uris.py
+++ trash/missingUris/add_uris.py
@@ -1,22 +1,22 @@
 COLUMNS = [
-    "plant",
-    "machinery",
-    "sequence",
-    "waste film",
-    "waste cardboard",
+    "plant name (data source)",
+    "http://data.europa.eu/xsp/cn2024/847410000080",
+    "sequence or step or ...",
+    "http://data.europa.eu/xsp/cn2024/392010230080",
+    "http://data.europa.eu/xsp/cn2024/470710000080",
     "http://data.europa.eu/xsp/cn2024/700100000080",
-    "waste paper",
+    "http://data.europa.eu/xsp/cn2024/470730000080",
     "http://data.europa.eu/xsp/cn2024/391510100080",
     "http://data.europa.eu/xsp/cn2024/391510200080",
     "http://data.europa.eu/xsp/cn2024/720400000080",
     "http://data.europa.eu/xsp/cn2024/760200110010",
-    "",
+    "http://data.europa.eu/ehl/cpa21/381131", # not in vocab yet; taxonomy can be accessed here: https://op.europa.eu/s/zXfR
 ]
 
 UNITS = [
     "https://www.w3.org/2001/XMLSchema#string",
-    "https://www.w3.org/2001/XMLSchema#string",
     "https://vocab.sentier.dev/units/unit/NUM",
+    "https://vocab.sentier.dev/units/quantity-kind/Count",
     "https://vocab.sentier.dev/units/unit/FRACTION",
     "https://vocab.sentier.dev/units/unit/FRACTION",
     "https://vocab.sentier.dev/units/unit/FRACTION",
