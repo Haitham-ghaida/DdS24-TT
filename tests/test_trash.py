f8e38a73f42fcda7331339749f1ae614b1f46d87
MiguelAstudillo <miguel.astudillo@lca-net.com>
2024-10-11 11:54:05 +0200

    update test

--- tests/test_trash.py
+++ tests/test_trash.py
@@ -2,6 +2,6 @@ import trash.mrf_model.flow_model as flowmodel
 
 def test_run():
 
-    psd = flowmodel.PlasticSD(year=[2020], verbose=1)
+    psd = flowmodel.PlasticSD(year=[2020], verbose=1,sample_size=2)
 
     demands, flows = psd.run()
